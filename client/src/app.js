05438f1259139cefde541e19398f4cbcb988fbb9
Yitzhak <idashevsky@gmail.com>
2016-11-13 21:50:51 +0200

    forgotten changes

--- client/src/app.js
+++ client/src/app.js
@@ -10,66 +10,66 @@ import {syncHistoryWithStore, routerReducer} from 'react-router-redux';
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
-//reducers.routing = routerReducer;
+reducers.routing = routerReducer;
 
 
-// import * as reducers from './reducers';
-// import App from './components/App';
-// import ShoppingList from "./components/ShoppingList";
-// /*
-//  state shape
-//  {
-//  shoppingList:[]
-//  }
-//  */
+import * as reducers from './reducers';
+import App from './components/App';
+import ShoppingList from "./components/ShoppingList";
+/*
+ state shape
+ {
+ shoppingList:[]
+ }
+ */
+
+const store = createStore(combineReducers(reducers));
+const history = syncHistoryWithStore(browserHistory, store);
+
+function run() {
+    var state = store.getState();
+    console.log(state);
+    ReactDOM.render((<Provider store={store}>
+            <Router history={history}>
+                <Route path="/" component={App}>
+                    <Route path="/shoppingList" component={ShoppingList}/>
+                    {/*<Route path="/map" component={MapComponent}/>*/}
+                </Route>
+            </Router>
+        </Provider>
+    ), document.getElementById('root'))
+}
+
+run();
+
+store.subscribe(run);
+
+// const toggleTodo = (todo) => {
+//     return {
+//         id: 0,
+//         text: "Learn Redux",
+//         completed: !todo.completed
+//     };
+// };
 //
-// const store = createStore(combineReducers(reducers));
-// const history = syncHistoryWithStore(browserHistory, store);
+// const testToggleTodo = () => {
+//     const todoBefore = {
+//         id: 0,
+//         text: "Learn Redux",
+//         completed: false
+//     };
+//     const todoAfter = {
+//         id: 0,
+//         text: "Learn Redux",
+//         completed: true
+//     };
 //
-// function run() {
-//     var state = store.getState();
-//     console.log(state);
-//     ReactDOM.render((<Provider store={store}>
-//             <Router history={history}>
-//                 <Route path="/" component={App}>
-//                     <Route path="/shoppingList" component={ShoppingList}/>
-//                     <Route path="/map" component={MapComponent}/>
-//                 </Route>
-//             </Router>
-//         </Provider>
-//     ), document.getElementById('root'))
-// }
+//     deepFreeze(todoBefore);
 //
-// run();
+//     expect(
+//         toggleTodo(todoBefore)
+//     ).toEqual(todoAfter);
+// };
 //
-// store.subscribe(run);
-
-const toggleTodo = (todo) => {
-    return {
-        id: 0,
-        text: "Learn Redux",
-        completed: !todo.completed
-    };
-};
-
-const testToggleTodo = () => {
-    const todoBefore = {
-        id: 0,
-        text: "Learn Redux",
-        completed: false
-    };
-    const todoAfter = {
-        id: 0,
-        text: "Learn Redux",
-        completed: true
-    };
-
-    deepFreeze(todoBefore);
-
-    expect(
-        toggleTodo(todoBefore)
-    ).toEqual(todoAfter);
-};
-
-testToggleTodo();
-console.log('All tests passed');
+// testToggleTodo();
+// console.log('All tests passed');
