b01fee4ce0268188862385e636007fb4f953a827
Yitzhak Dashevsky <yitzhak@razorgrip.com>
2017-03-30 20:17:43 +0300

    remove fu..ing theme

--- ng-app/src/app/sidebar/sidebar.component.ts
+++ ng-app/src/app/sidebar/sidebar.component.ts
@@ -4,25 +4,25 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'sidebar',
   template: `
-<div class="sidebar">
-        <ul>
-            <li>
-                <a md-button routerLink="/shopping-list">shopping list</a>
-            </li>
-            <li>
-                <a md-button routerLink="/map">map</a>
-            </li>
-            <li>
-                <a md-button routerLink="/pin-place">pin place</a>
-            </li>
-        </ul>
+    <div class="sidebar">
+      <ul>
+        <li>
+          <a routerLink="/shopping-list">shopping list</a>
+        </li>
+        <li>
+          <a routerLink="/map">map</a>
+        </li>
+        <li>
+          <a routerLink="/pin-place">pin place</a>
+        </li>
+      </ul>
     </div>
-`
+  `
 })
 export class SidebarComponent implements OnInit {
   constructor() {
   }
-
+ 
   ngOnInit() {
   }
 
