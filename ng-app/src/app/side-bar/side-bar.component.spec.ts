707118dc46add1e38e05cd74f5218d754ef0eedd
Yitzhak Dashevsky <yitzhak@razorgrip.com>
2017-08-05 23:16:31 +0300

    fix side bar spec

--- ng-app/src/app/side-bar/side-bar.component.spec.ts
+++ ng-app/src/app/side-bar/side-bar.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+//
+import { RouterTestingModule } from '@angular/router/testing';
+//
 import { SideBarComponent } from './side-bar.component';
 
 describe('SideBarComponent', () => {
@@ -8,6 +10,9 @@ describe('SideBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[
+        RouterTestingModule
+      ],
       declarations: [ SideBarComponent ]
     })
     .compileComponents();
