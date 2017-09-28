14e1de7e7d7f10f4949d90d034c3712c28dde2e6
Yitzhak Dashevsky <yitzhak@razorgrip.com>
2017-09-28 21:48:32 +0300

    move parser to separate file

--- server/src/helpers/parser.ts
+++ server/src/helpers/parser.ts
@@ -3,31 +3,9 @@ import { PinPlaceRequest, WishItemRequest } from './request-body.validator';
 import 'reflect-metadata';
 import { injectable } from 'inversify';
 import { Place } from '../models/place.model';
+import { parseAddPlaceRequest, parseAddWishItemRequest } from './parsers';
 
-/***
- * parsers
- */
-const parseAddWishItemRequest: ParserFn = (obj: WishItemRequest): WishItem => {
-    return {
-        ...new WishItem(),
-        completed: obj.completed || false,
-        id: obj.id,
-        indexNum: obj.indexNum,
-        name: obj.name,
-    };
-};
-
-const parseAddPlaceRequest: ParserFn = (obj: PinPlaceRequest): Place => {
-    return {
-        ...new Place(),
-        location: `${obj.location.coordinates[0]},${obj.location.coordinates[1]}`,
-        name: obj.name,
-        description: obj.description || '',
-        tags: obj.tags || [],
-    };
-};
-
-type ParserFn = (o: ParseSource) => ParseTarget;
+export type ParserFn = (o: ParseSource) => ParseTarget;
 
 declare type ParseSource = WishItemRequest | PinPlaceRequest;
 declare type ParseTarget = WishItem | Place;
